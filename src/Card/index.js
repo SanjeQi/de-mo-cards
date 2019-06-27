702597c346a9c13778638b61171b26642c4f76dd
github username <cristi.nord@gmail.com>
2019-06-27 19:46:49 +0100

    user_carsd_1

---
+++ src/Card/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./styles.scss";
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let { href } = this.props;
+
+    href = href !== undefined ? href : null;
+
+    return (
+      <a href={href} className="card-link">
+        <div className="card">{this.props.children}</div>
+      </a>
+    );
+  }
+}
+
+export default Card;
---
+++ src/UserCard/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Card from "../Card";
+import UserCardHeader from "./UserCardHeader";
+import UserCardAvatar from "./UserCardAvatar";
+import UserCardBody from "./UserCardBody";
+import UserCardStats from "./UserCardStats";
+
+class UserCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.props);
+    let { href, header, avatar, name, positionName, stats } = this.props;
+
+    return (
+      <Card href={href}>
+        <UserCardHeader src={header} />
+        <UserCardAvatar src={avatar} />
+        <UserCardBody name={name} positionName={positionName}>
+          {this.props.children}
+        </UserCardBody>
+
+        {stats !== undefined ? <UserCardStats stats={stats} /> : null}
+      </Card>
+    );
+  }
+}
+
+export default UserCard;
