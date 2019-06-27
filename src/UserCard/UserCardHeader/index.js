6cd219ec1b44a2c1318e03c70f93c2ce24b8e75f
github username <cristi.nord@gmail.com>
2019-06-27 22:44:31 +0100

    UserCardHeader

---
+++ src/UserCard/UserCardHeader/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./styles.scss";
+
+class UserCardHeader extends React.Component {
+  render() {
+    let { src } = this.props;
+
+    return (
+      <div
+        className="user-card-header"
+        style={{ backgroundImage: `url(${src})` }}
+      />
+    );
+  }
+}
+
+export default UserCardHeader;
