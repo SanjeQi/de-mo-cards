8d2df0a26e9ce12b4ecc6b6ebe8b925b0cf438a3
github username <cristi.nord@gmail.com>
2019-06-27 21:00:08 +0100

    UserCardAvatar

---
+++ src/UserCard/UserCardAvatar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./styles.scss";
+
+class UserCardAvatar extends React.Component {
+  render() {
+    let { src } = this.props;
+
+    return (
+      <div
+        className="user-card-avatar"
+        style={{ backgroundImage: `url(${src})` }}
+      />
+    );
+  }
+}
+
+export default UserCardAvatar;
