e2ef2b3bded572505b1c02cb0b67fa667ec17143
github username <cristi.nord@gmail.com>
2019-07-03 14:08:35 +0100

    Add tagged content card_2 nas styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import React from "react";
+import UserCard from "./UserCard";
+import ProductCard from "./ProductCard";
+import TaggedContentCard from "./TaggedContentCard";
 
-class MyComponent extends React.Component {
-  render() {
-    return <div>My simple React component</div>;
-  }
-}
-
-export default MyComponent;
+export { UserCard, ProductCard, TaggedContentCard };
