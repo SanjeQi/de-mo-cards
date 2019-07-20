fbd5dbe306e6f22860a6b75e4af58f43fd7f2f8c
github username <cristi.nord@gmail.com>
2019-07-20 20:24:32 +0100

    Chip

---
+++ src/PaymentCard/Chip/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./styles.scss";
+
+const Chip = props => {
+  return <span className="chip" />;
+};
+
+Chip.propTypes = {};
+
+export default Chip;
