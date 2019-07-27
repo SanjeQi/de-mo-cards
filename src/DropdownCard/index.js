6f8f3b4379aa95e93db4377a4e599db257b30927
github username <cristi.nord@gmail.com>
2019-07-27 20:58:27 +0100

    DropdownCard component from subcomponents

---
+++ src/DropdownCard/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Card from "../Card";
+import NameSurname from "./NameSurname";
+import Arrow from "./Arrow";
+import ToggledCard from "./ToggledCard";
+
+import styles from "./styles.scss";
+
+class DropdownCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Card className="dropdown-card">
+        <NameSurname />
+        <Arrow />
+        <ToggledCard />
+      </Card>
+    );
+  }
+}
+export default DropdownCard;
